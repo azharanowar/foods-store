515124e3b17f06047b6a209deee2fc10a1013588
azharanowar <azharanowar@gmail.com>
2022-09-26 21:35:28 +0600

    Load foods/meals from TheMealDB API and display foods cards with bootstrap 5

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+const getFoodsByDynamicURL = (url) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayFoods(data))
+}
+
+const displayFoods = data => {
+    const meals = data.meals;
+    const foodsCards = document.getElementById("foodsCard");
+    meals.forEach(meal => {
+        const mealId = meal.idMeal;
+        const mealName = meal.strMeal;
+        const mealShortDescription = meal.strInstructions.slice(0, 120);
+        const mealImage = meal.strMealThumb;
+        const mealCategory = meal.strCategory;
+        const mealAvailableArea = meal.strArea;
+        
+        const newFoodCard = document.createElement('div');
+        newFoodCard.classList.add('col');
+        newFoodCard.innerHTML = `<div class="card h-100 text-center shadow-sm">
+                <img src="${mealImage}" class="card-img-top" alt="${mealName} Image">
+                <div class="card-body pb-4 pt-3 px-3">
+                    <h5 class="fs-4 card-title">${mealName}</h5>
+                    <p class="card-text">${mealShortDescription}</p>
+                    <p class="text-muted"><span>Category: ${mealCategory}</span> | <span>Area: ${mealAvailableArea}</span></p>
+                    <button type="button" class="btn btn-success rounded-2 py-2 w-100" onclick="foodSinglePageInformation()">Learn More</button>
+                </div>
+            </div>`;
+        
+        foodsCards.appendChild(newFoodCard);
+
+    });
+}
+
+const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
+getFoodsByDynamicURL(url);
+
+
+
